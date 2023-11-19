ab4a40e75aea2ac9d2ffc5dc0e3389091db77bf9
Clichee <timur_aydin.33@hotmail.de>
2023-11-19 18:38:25 +0100

    Create file for handling climate data

---
+++ public/libs/earth/1.0.0/climate.js
@@ -0,0 +1,16 @@
+/**
+ * This file is part of the climate-earth fork.
+ * https://github.com/Clichee/climate-earth
+ * 
+ * Handling everything around the climate (.nc) data.
+ * 
+ * This file will call the JSON Files, 
+ * which were converted to json from NetCDF,
+ * and extract their relevant information
+ * to project it onto the map.
+ * 
+ * Not sure how much of that will be in this file for now.
+ * Also, files will be called locally for now.
+ * Migration to call from an API (and trigger the conversen process)
+ * will be done later
+ */
